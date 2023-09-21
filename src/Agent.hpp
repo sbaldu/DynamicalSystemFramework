b5939b915a74e5c2508c2b425c860f96f2150e87
sbaldu <simone.balducci00@gmail.com>
2023-09-21 22:48:02 +0200

    Add Agent class

---
+++ src/Agent.hpp
@@ -0,0 +1,94 @@
+
+#ifndef Agent_hpp
+#define Agent_hpp
+
+#include "Itinerary.hpp"
+#include "SparseMatrix.hpp"
+
+namespace dmf {
+
+  template <typename Id, typename Weight>
+  class Agent {
+  private:
+    Id m_index;
+    Id m_position;
+    Id m_previousPosition;
+    Itinerary<Id> m_itinerary;
+    SparseMatrix<Id, Weight> m_transitionMatrix;
+
+  public:
+    Agent() = default;
+    Agent(Id index, Id position);
+    Agent(Id index, Id position, Itinerary<Id> itinerary);
+    Agent(Id index, Id position, Itinerary<Id> itinerary, SparseMatrix<Id, Weight> matrix);
+
+    // Setters
+    void setPosition(Id position);
+    void setItinerary(Itinerary<Id> itinerary);
+    void setTransitionMatrix(SparseMatrix<Id, Weight> matrix);
+
+    // Getters
+    int index() const;
+    int position() const;
+    int previousPosition() const;
+    const Itinerary<Id>& itinerary() const;
+    const SparseMatrix<Id, Weight>& transitionMatrix() const;
+  };
+
+  template <typename Id, typename Weight>
+  Agent<Id, Weight>::Agent(Id index, Id position)
+      : m_index{index}, m_position{position}, m_previousPosition{position} {}
+
+  template <typename Id, typename Weight>
+  Agent<Id, Weight>::Agent(Id index, Id position, Itinerary<Id> itinerary)
+      : m_index{index},
+        m_position{position},
+        m_previousPosition{position},
+        m_itinerary{std::move(itinerary)} {}
+
+  template <typename Id, typename Weight>
+  Agent<Id, Weight>::Agent(Id index, Id position, Itinerary<Id> itinerary, SparseMatrix<Id, Weight> matrix)
+      : m_index{index},
+        m_position{position},
+        m_previousPosition{position},
+        m_itinerary{std::move(itinerary)},
+        m_transitionMatrix{std::move(matrix)} {}
+
+  // Setters
+  template <typename Id, typename Weight>
+  void Agent<Id, Weight>::setPosition(Id position) {
+    m_position = position;
+  }
+  template <typename Id, typename Weight>
+  void Agent<Id, Weight>::setItinerary(Itinerary<Id> itinerary) {
+    m_itinerary = std::move(itinerary);
+  }
+  template <typename Id, typename Weight>
+  void Agent<Id, Weight>::setTransitionMatrix(SparseMatrix<Id, Weight> matrix) {
+    m_transitionMatrix = std::move(matrix);
+  }
+
+  // Getters
+  template <typename Id, typename Weight>
+  int Agent<Id, Weight>::index() const {
+    return m_index;
+  }
+  template <typename Id, typename Weight>
+  int Agent<Id, Weight>::position() const {
+    return m_position;
+  }
+  template <typename Id, typename Weight>
+  int Agent<Id, Weight>::previousPosition() const {
+    return m_previousPosition;
+  }
+  template <typename Id, typename Weight>
+  const Itinerary<Id>& Agent<Id, Weight>::itinerary() const {
+    return m_itinerary;
+  }
+  template <typename Id, typename Weight>
+  const SparseMatrix<Id, Weight>& Agent<Id, Weight>::transitionMatrix() const {
+    return m_transitionMatrix;
+  }
+};  // namespace dmf
+
+#endif
