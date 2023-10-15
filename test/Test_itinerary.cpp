bfbdfa5ef06893f8e3a056c3e4f63118dd413fb7
Grufoony <gregorio_2000@hotmail.it>
2023-10-15 11:38:27 +0200

    added some tests

---
+++ test/Test_itinerary.cpp
@@ -0,0 +1,30 @@
+#include "../src/Itinerary.hpp"
+#include <cstdint>
+
+#include "doctest.h"
+
+using Itinerary = dmf::Itinerary<uint16_t>;
+
+TEST_CASE("Itinerary") {
+  SUBCASE("Constructor") {
+    /*This tests the constructor that takes two Ids.
+    GIVEN: Two Ids
+    WHEN: An Itinerary is constructed
+    THEN: The source and destination are set correctly
+    */
+    Itinerary itinerary{1, 2};
+    CHECK(itinerary.source() == 1);
+    CHECK(itinerary.destination() == 2);
+  }
+  SUBCASE("Copy constructor") {
+    /*This tests the copy constructor.
+    GIVEN: An Itinerary
+    WHEN: An Itinerary is constructed from the first Itinerary
+    THEN: The source and destination are set correctly
+    */
+    Itinerary itinerary{1, 2};
+    Itinerary copy{itinerary};
+    CHECK(copy.source() == 1);
+    CHECK(copy.destination() == 2);
+  }
+}
