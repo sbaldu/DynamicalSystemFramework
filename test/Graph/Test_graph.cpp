b86e275b260f1cb99c797604909135637a45a523
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-10-18 21:22:26 +0200

    add graph test

--- test/Graph/Test_graph.cpp
+++ test/Graph/Test_graph.cpp
@@ -3,7 +3,6 @@
 #include "Graph.hpp"
 #include "Node.hpp"
 #include "Street.hpp"
-#include "HashFunctions.hpp"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
@@ -12,14 +11,14 @@ using Graph = dmf::Graph<uint16_t, uint16_t>;
 using Street = dmf::Street<uint16_t, uint16_t>;
 
 TEST_CASE("Graph") {
-    SUBCASE("Constructor") {
-        Street street{1, 2, 3};
-        Graph graph{};
-        graph.addStreet(street);
-        graph.buildAdj();
-        CHECK(graph.streetSet().size() == 1);
-        CHECK(graph.adjMatrix()->size() == 2);
-        // CHECK(graph.adjMatrix()->contains(0, 1));
-        // CHECK(graph.adjMatrix()->contains(1, 0));
-    }
+  SUBCASE("Constructor") {
+    Street street{1, 2, 3};
+    Graph graph{};
+    graph.addStreet(street);
+    graph.buildAdj();
+    CHECK(graph.streetSet().size() == 1);
+    CHECK(graph.adjMatrix()->size() == 1);
+    // CHECK(graph.adjMatrix()->contains(1, 2));
+    // CHECK(graph.adjMatrix()->contains(2, 1));
+  }
 }
