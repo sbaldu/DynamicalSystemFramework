ebd16a22109ee9910d5f417981fe40c8903a67f6
sbaldu <simone.balducci00@gmail.com>
2023-10-20 12:05:08 +0200

    Formatting

--- src/utility/TypeTraits/is_numeric.hpp
+++ src/utility/TypeTraits/is_numeric.hpp
@@ -13,12 +13,12 @@ namespace dmf {
 
   // define is_numeric_v type trait
   template <typename T>
-  struct is_numeric : std::is_arithmetic<T>{};
+  struct is_numeric : std::is_arithmetic<T> {};
 
   template <>
-  struct is_numeric<bool> : std::false_type{};
+  struct is_numeric<bool> : std::false_type {};
   template <>
-  struct is_numeric<char> : std::false_type{};
+  struct is_numeric<char> : std::false_type {};
 
   template <typename T>
   inline constexpr bool is_numeric_v = is_numeric<T>::value;
