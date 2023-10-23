33cb324f93a3469a9a4dfa7e330d10a5230a6e1a
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-10-23 22:05:04 +0200

    reworked `Itinerary`

--- src/Itinerary.hpp
+++ src/Itinerary.hpp
@@ -2,30 +2,76 @@
 #ifndef Itinerary_hpp
 #define Itinerary_hpp
 
+#include "SparseMatrix.hpp"
+
 #include <concepts>
 #include <utility>
 
 namespace dmf {
 
   template <typename Id>
-    requires std::unsigned_integral<Id>
+  requires std::unsigned_integral<Id>
   class Itinerary {
   private:
     std::pair<Id, Id> m_trip;
+    SparseMatrix<Id, bool> m_path;
 
   public:
     Itinerary() = default;
-    Itinerary(Id source, Id destination) : m_trip{std::make_pair(source, destination)} {}
+    Itinerary(Id source, Id destination);
     explicit Itinerary(std::pair<Id, Id> trip) : m_trip{std::move(trip)} {}
+    Itinerary(Id source, Id destination, SparseMatrix<Id, bool> path);
+    Itinerary(std::pair<Id, Id> trip, SparseMatrix<Id, bool> path);
 
     // Setters
-    void set_source(Id source) { m_trip.first = source; }
-    void set_destination(Id destination) { m_trip.second = destination; }
+    void setSource(Id source);
+    void setDestination(Id destination);
+    void setPath(SparseMatrix<Id, bool> path);
 
     // Getters
-    Id source() const { return m_trip.first; }
-    Id destination() const { return m_trip.second; }
+    Id source() const;
+    Id destination() const;
+    std::pair<Id, Id> trip() const;
+    SparseMatrix<Id, bool> path() const;
   };
+  template <typename Id>
+  requires std::unsigned_integral<Id> Itinerary<Id>::Itinerary(Id source, Id destination)
+      : m_trip{std::make_pair(source, destination)} {}
+  template <typename Id>
+  requires std::unsigned_integral<Id> Itinerary<Id>::Itinerary(Id source,
+                                                               Id destination,
+                                                               SparseMatrix<Id, bool> path)
+      : m_trip{std::make_pair(source, destination)}, m_path{std::move(path)} {}
+  template <typename Id>
+  requires std::unsigned_integral<Id> Itinerary<Id>::Itinerary(std::pair<Id, Id> trip,
+                                                               SparseMatrix<Id, bool> path)
+      : m_trip{std::move(trip)}, m_path{std::move(path)} {}
+
+  //Setters
+  template <typename Id>
+  requires std::unsigned_integral<Id>
+  void Itinerary<Id>::setSource(Id source) { m_trip.first = source; }
+  template <typename Id>
+  requires std::unsigned_integral<Id>
+  void Itinerary<Id>::setDestination(Id destination) { m_trip.second = destination; }
+  template <typename Id>
+  requires std::unsigned_integral<Id>
+  void Itinerary<Id>::setPath(SparseMatrix<Id, bool> path) { m_path = std::move(path); }
+
+  //Getters
+  template <typename Id>
+  requires std::unsigned_integral<Id> Id Itinerary<Id>::source()
+  const { return m_trip.first; }
+  template <typename Id>
+  requires std::unsigned_integral<Id> Id Itinerary<Id>::destination()
+  const { return m_trip.second; }
+  template <typename Id>
+  requires std::unsigned_integral<Id> std::pair<Id, Id> Itinerary<Id>::trip()
+  const { return m_trip; }
+  template <typename Id>
+  requires std::unsigned_integral<Id> SparseMatrix<Id, bool> Itinerary<Id>::path()
+  const { return m_path; }
+
 };  // namespace dmf
 
 #endif
--- test/Itinerary/Test_itinerary.cpp
+++ test/Itinerary/Test_itinerary.cpp
@@ -8,7 +8,7 @@
 using Itinerary = dmf::Itinerary<uint16_t>;
 
 TEST_CASE("Itinerary") {
-  SUBCASE("Constructor") {
+  SUBCASE("Constructor_1") {
     /*This tests the constructor that takes two Ids.
     GIVEN: Two Ids
     WHEN: An Itinerary is constructed
@@ -17,6 +17,40 @@ TEST_CASE("Itinerary") {
     Itinerary itinerary{1, 2};
     CHECK(itinerary.source() == 1);
     CHECK(itinerary.destination() == 2);
+  }
+  SUBCASE("Constructor_2") {
+    /*This tests the constructor that takes a pair of Ids.
+    GIVEN: A pair of Ids
+    WHEN: An Itinerary is constructed
+    THEN: The source and destination are set correctly
+    */
+    Itinerary itinerary{std::pair{1, 2}};
+    CHECK(itinerary.source() == 1);
+    CHECK(itinerary.destination() == 2);
+  }
+  SUBCASE("Constructor_3") {
+    /*This tests the constructor that takes two Ids and a SparseMatrix.
+    GIVEN: Two Ids and a SparseMatrix
+    WHEN: An Itinerary is constructed
+    THEN: The source, destination, and path are set correctly
+    */
+    Itinerary itinerary{1, 2, dmf::SparseMatrix<uint16_t, bool>{1, 1}};
+    CHECK(itinerary.source() == 1);
+    CHECK(itinerary.destination() == 2);
+    CHECK(itinerary.path().getRowDim() == 1);
+    CHECK(itinerary.path().getColDim() == 1);
+  }
+  SUBCASE("Constructor_4") {
+    /*This tests the constructor that takes a pair of Ids and a SparseMatrix.
+    GIVEN: A pair of Ids and a SparseMatrix
+    WHEN: An Itinerary is constructed
+    THEN: The source, destination, and path are set correctly
+    */
+    Itinerary itinerary{std::pair{1, 2}, dmf::SparseMatrix<uint16_t, bool>{1, 1}};
+    CHECK(itinerary.source() == 1);
+    CHECK(itinerary.destination() == 2);
+    CHECK(itinerary.path().getRowDim() == 1);
+    CHECK(itinerary.path().getColDim() == 1);
   }
   SUBCASE("Copy constructor") {
     /*This tests the copy constructor.
