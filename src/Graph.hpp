3ab2c5548f9737d16f9fb4f5ce215b46d0c96fdf
sbaldu <simone.balducci00@gmail.com>
2023-10-28 20:43:10 +0200

    Fix missing reference in Graph constructor

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -35,7 +35,7 @@ namespace dmf {
 
   public:
     Graph();
-    Graph(const SparseMatrix<Id, bool> adj);
+    Graph(const SparseMatrix<Id, bool>& adj);
     Graph(const std::unordered_set<shared<Street<Id, Size>>, nodeHash<Id>>& streetSet);
 
     void buildAdj();
@@ -74,7 +74,7 @@ namespace dmf {
 
   template <typename Id, typename Size>
     requires(std::unsigned_integral<Id> && std::unsigned_integral<Size>)
-  Graph<Id, Size>::Graph(const SparseMatrix<Id, bool> adj)
+  Graph<Id, Size>::Graph(const SparseMatrix<Id, bool>& adj)
       : m_adjacency{make_shared<SparseMatrix<Id, bool>>(adj)} {
     std::ranges::for_each(std::views::iota(0, (int)adj.getColDim()),
                           [this](auto i) -> void { m_nodes.insert(make_shared<Node<Id>>(i)); });
