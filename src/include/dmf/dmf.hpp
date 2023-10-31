5fb3fe7ac5ec229212b09252ec8da1a608195307
sbaldu <simone.balducci00@gmail.com>
2023-10-31 19:39:07 +0100

    Add `dmf.hpp` header including all the others

---
+++ src/include/dmf/dmf.hpp
@@ -0,0 +1,16 @@
+
+#ifndef dmf_hpp
+#define dmf_hpp
+
+#include "./Agent.hpp"
+#include "./Graph.hpp"
+#include "./Itinerary.hpp"
+#include "./Node.hpp"
+#include "./SparseMatrix.hpp"
+#include "./Street.hpp"
+#include "./utility/HashFunctions.hpp"
+#include "./utility/TypeTraits/is_node.hpp"
+#include "./utility/TypeTraits/is_street.hpp"
+#include "./utility/TypeTraits/is_numeric.hpp"
+
+#endif
