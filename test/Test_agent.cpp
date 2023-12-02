6d2e7b57d2b8d30433342b742441512a33b30281
Gregorio Berselli <gregorio_2000@hotmail.it>
2023-12-02 17:42:17 +0100

    Reworked Itinerary Id (#78)
    * Refactor Agent class constructor and add
    itineraryId parameter
    
    * Update Agent constructor to include a new
    parameter
    ---------
    
    Co-authored-by: sbaldu <simone.balducci00@gmail.com>

--- test/Test_agent.cpp
+++ test/Test_agent.cpp
@@ -10,18 +10,10 @@ using Itinerary = dsm::Itinerary<uint16_t>;
 
 TEST_CASE("Agent") {
   SUBCASE("Constructor_1") {
-    Agent agent{1, 1};
+    Agent agent{1, 1, 0};
     CHECK_EQ(agent.index(), 1);
     CHECK_EQ(agent.streetId(), 1);
-    CHECK_EQ(agent.speed(), 0);
-    CHECK_EQ(agent.delay(), 0);
-    CHECK_EQ(agent.time(), 0);
-  }
-  SUBCASE("Constructor_2") {
-    Itinerary itinerary{0, 1};
-    Agent agent{1, 1, itinerary};
-    CHECK_EQ(agent.index(), 1);
-    CHECK_EQ(agent.streetId(), 1);
+    CHECK_EQ(agent.itineraryId(), 0);
     CHECK_EQ(agent.speed(), 0);
     CHECK_EQ(agent.delay(), 0);
     CHECK_EQ(agent.time(), 0);
