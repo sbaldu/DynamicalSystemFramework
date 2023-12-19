11f3645f242e516344bbf8f63d5361f8f5e7afa9
Gregorio Berselli <gregorio.berselli@studio.unibo.it>
2023-12-19 17:13:14 +0100

    Remove unused docs fodler (#102)

--- docs/search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['capacity_2',['capacity',['../classdsm_1_1Node.html#a74af84dd8e2a67c7a79cecf952443e7a',1,'dsm::Node']]],
-  ['clear_3',['clear',['../classdsm_1_1SparseMatrix.html#a48287852ed2a75ec39a20ba62603965f',1,'dsm::SparseMatrix']]],
-  ['contains_4',['contains',['../classdsm_1_1SparseMatrix.html#a57384e33f75162bf97d464f1a73b7275',1,'dsm::SparseMatrix::contains(Index i, Index j) const'],['../classdsm_1_1SparseMatrix.html#ab553af75991a9a071706d9c7442bee42',1,'dsm::SparseMatrix::contains(Index const index) const']]],
-  ['coords_5',['coords',['../classdsm_1_1Node.html#a7f004b4856ea1db25fdfc6beff7b553c',1,'dsm::Node']]]
-];
