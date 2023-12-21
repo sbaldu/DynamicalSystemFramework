4039abfa705a43bcd57a3e329d24596907ef81ba
sbaldu <simone.balducci00@gmail.com>
2023-12-21 12:34:35 +0100

    Implement `dsm::priority_queue`

--- src/dsm/utility/queue.hpp
+++ src/dsm/utility/queue.hpp
@@ -8,19 +8,35 @@ namespace dsm {
   // use the default allocator for std::deque
   template <typename T, typename Container = std::deque<T>>
   class queue : public std::queue<T, Container> {
-	public:
-	  typedef typename Container::iterator iterator;
-	  typedef typename Container::const_iterator const_iterator;
+  public:
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
 
-	  // c is a protected member of std::queue, which is the underlying container
-	  iterator begin() { return this->c.begin(); }
-	  iterator end() { return this->c.end(); }
-	  const_iterator begin() const { return this->c.begin(); }
-	  const_iterator end() const { return this->c.end(); }
+    // c is a protected member of std::queue, which is the underlying container
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
 
-	  T operator[](size_t i) { return *(this->c.begin() + i); }
-	  const T operator[](size_t i) const { return *(this->c.begin() + i); }
+    T operator[](size_t i) { return *(this->c.begin() + i); }
+    const T operator[](size_t i) const { return *(this->c.begin() + i); }
   };
-};
+
+  template <typename T,
+            typename Container = std::vector<T>,
+            typename Compare = std::less<typename Container::value_type>>
+  class priority_queue : public std::priority_queue<T, Container, Compare> {
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+
+    // c is a protected member of std::queue, which is the underlying container
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+
+    T operator[](size_t i) { return *(this->c.begin() + i); }
+    const T operator[](size_t i) const { return *(this->c.begin() + i); }
+  };
 
 #endif
