5b01e7ed37dcfb05a0d0518ebdae6470c5266fb0
Gregorio Berselli <gregorio.berselli@studio.unibo.it>
2023-12-25 12:36:22 +0100

    Optimize `updatePath()` function (#104)
    * Optimize `updatePaths()`
    
    * Add paths to paths map in Dynamics.hpp
    
    * Fix issue with setting path in Dynamics.hpp
    
    * benchmarking
    
    * Formatting
    
    ---------
    
    Co-authored-by: sbaldu <simone.balducci00@gmail.com>

--- src/dsm/headers/Dynamics.hpp
+++ src/dsm/headers/Dynamics.hpp
@@ -253,7 +253,13 @@ namespace dsm {
              is_numeric_v<Delay>)
   void Dynamics<Id, Size, Delay>::updatePaths() {
     const Size dimension = m_graph->adjMatrix()->getRowDim();
+    std::unordered_map<Id, SparseMatrix<Id, bool>> paths;
     for (auto& itineraryPair : m_itineraries) {
+      if (this->m_time == 0 && itineraryPair.second->path().size() == 0 &&
+          paths.contains(itineraryPair.second->destination())) {
+        itineraryPair.second->setPath(paths.at(itineraryPair.second->destination()));
+        continue;
+      }
       SparseMatrix<Id, bool> path{dimension, dimension};
       // cycle over the nodes
       for (Size i{0}; i < dimension; ++i) {
@@ -292,6 +298,7 @@ namespace dsm {
           }
         }
         itineraryPair.second->setPath(path);
+        paths.emplace(itineraryPair.second->destination(), path);
       }
     }
   }
