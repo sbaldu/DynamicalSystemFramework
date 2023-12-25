6ddd677e5e6e5b660c960d0e147b72538b9dd7d3
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-12-25 19:39:37 +0100

    Add m_transportCapacity parameter

--- src/dsm/headers/Nodes/Node.hpp
+++ src/dsm/headers/Nodes/Node.hpp
@@ -28,6 +28,7 @@ namespace dsm {
     std::pair<double, double> m_coords;
     Id m_id;
     Size m_capacity;
+    Size m_transportCapacity;
 
   public:
     Node() = delete;
@@ -45,6 +46,11 @@ namespace dsm {
     /// @brief Set the node's capacity
     /// @param capacity The node's capacity
     void setCapacity(Size capacity);
+    /// @brief Set the node's transport capacity
+    /// @details The transport capacity is the maximum number of agents that can traverse the node
+    ///          in a time step.
+    /// @param capacity The node's transport capacity
+    void setTransportCapacity(Size capacity);
 
     /// @brief Get the node's id
     /// @return Id The node's id
@@ -55,6 +61,11 @@ namespace dsm {
     /// @brief Get the node's capacity
     /// @return Size The node's capacity
     Size capacity() const;
+    /// @brief Get the node's transport capacity
+    /// @details The transport capacity is the maximum number of agents that can traverse the node
+    ///          in a time step.
+    /// @return Size The node's transport capacity
+    Size transportCapacity() const;
 
     /// @brief Returns true if the node is full
     /// @return bool True if the node is full
@@ -67,11 +78,11 @@ namespace dsm {
 
   template <typename Id, typename Size>
     requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
-  Node<Id, Size>::Node(Id id) : m_id{id}, m_capacity{1} {}
+  Node<Id, Size>::Node(Id id) : m_id{id}, m_capacity{1}, m_transportCapacity{std::numeric_limits<Size>::max()} {}
   template <typename Id, typename Size>
     requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
   Node<Id, Size>::Node(Id id, std::pair<double, double> coords)
-      : m_coords{coords}, m_id{id}, m_capacity{1} {}
+      : m_coords{coords}, m_id{id}, m_capacity{1}, m_transportCapacity{std::numeric_limits<Size>::max()} {}
 
   template <typename Id, typename Size>
     requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
@@ -82,6 +93,11 @@ namespace dsm {
     requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
   void Node<Id, Size>::setCapacity(Size capacity) {
     m_capacity = capacity;
+  }
+  template <typename Id, typename Size>
+    requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
+  void Node<Id, Size>::setTransportCapacity(Size capacity) {
+    m_transportCapacity = capacity;
   }
 
   template <typename Id, typename Size>
@@ -98,6 +114,11 @@ namespace dsm {
     requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
   Size Node<Id, Size>::capacity() const {
     return m_capacity;
+  }
+  template <typename Id, typename Size>
+    requires std::unsigned_integral<Id> && std::unsigned_integral<Size>
+  Size Node<Id, Size>::transportCapacity() const {
+    return m_transportCapacity;
   }
 
 };  // namespace dsm
