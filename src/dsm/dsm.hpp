1cc62d9e20a250edabdf9eff46ca214a959b58a0
Grufoony <64806874+Grufoony@users.noreply.github.com>
2024-03-27 21:41:11 +0100

    Update version

--- src/dsm/dsm.hpp
+++ src/dsm/dsm.hpp
@@ -5,7 +5,7 @@
 
 static constexpr uint8_t DSM_VERSION_MAJOR = 1;
 static constexpr uint8_t DSM_VERSION_MINOR = 3;
-static constexpr uint8_t DSM_VERSION_PATCH = 0;
+static constexpr uint8_t DSM_VERSION_PATCH = 1;
 
 #include <string>
 
