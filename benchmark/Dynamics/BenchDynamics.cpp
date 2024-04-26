f4df347f0668bd4e6bf1198c22d4847d3113f75c
Grufoony <64806874+Grufoony@users.noreply.github.com>
2024-04-26 15:42:21 +0200

    Bugfix in SparseMatrix and working benchmarks

--- benchmark/Dynamics/BenchDynamics.cpp
+++ benchmark/Dynamics/BenchDynamics.cpp
@@ -12,48 +12,22 @@ using Dynamics = dsm::FirstOrderDynamics<uint32_t, uint32_t, uint32_t>;
 using Bench = sb::Bench<long long int>;
 
 int main() {
-
-    Graph graph{};
+  Graph graph{};
   graph.importMatrix("../test/data/matrix.dat", false);
-  for (auto &streetPair : graph.streetSet()) {
-    auto street = streetPair.second;
+  for (const auto& [streetId, street] : graph.streetSet()) {
     street->setMaxSpeed(13.9);
   }
 
-  Itinerary it1{0, 10, 118};
-  Itinerary it2{1, 7, 118};
-  Itinerary it3{2, 4, 118};
-  Itinerary it4{3, 1, 118};
-  Itinerary it5{4, 10, 115};
-  Itinerary it6{5, 7, 115};
-  Itinerary it7{6, 4, 115};
-  Itinerary it8{7, 1, 115};
-  Itinerary it9{8, 10, 112};
-  Itinerary it10{9, 7, 112};
-  Itinerary it11{10, 4, 112};
-  Itinerary it12{11, 1, 112};
-  Itinerary it13{12, 10, 109};
-  Itinerary it14{13, 7, 109};
-  Itinerary it15{14, 4, 109};
-  Itinerary it16{15, 1, 109};
+  Itinerary it1{0, 118};
+  Itinerary it2{4, 115};
+  Itinerary it3{8, 112};
+  Itinerary it4{12, 109};
 
   Dynamics dynamics{graph};
   dynamics.addItinerary(it1);
   dynamics.addItinerary(it2);
   dynamics.addItinerary(it3);
   dynamics.addItinerary(it4);
-  dynamics.addItinerary(it5);
-  dynamics.addItinerary(it6);
-  dynamics.addItinerary(it7);
-  dynamics.addItinerary(it8);
-  dynamics.addItinerary(it9);
-  dynamics.addItinerary(it10);
-  dynamics.addItinerary(it11);
-  dynamics.addItinerary(it12);
-  dynamics.addItinerary(it13);
-  dynamics.addItinerary(it14);
-  dynamics.addItinerary(it15);
-  dynamics.addItinerary(it16);
   dynamics.setSeed(69);
   dynamics.setErrorProbability(0.3);
   dynamics.setMinSpeedRateo(0.95);
