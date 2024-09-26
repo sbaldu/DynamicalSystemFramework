bd998e5253d1ca6906031b8a99cae7da719ebb53
Simone Balducci <simone.balducci00@gmail.com>
2024-09-26 11:50:22 +0200

    Extend `is_node` to Node derivatives

--- src/dsm/utility/TypeTraits/is_node.hpp
+++ src/dsm/utility/TypeTraits/is_node.hpp
@@ -10,6 +10,19 @@ namespace dsm {
     requires(std::unsigned_integral<Id> && std::unsigned_integral<Size>)
   class Node;
 
+  template <typename Id, typename Size>
+	requires(std::unsigned_integral<Id> && std::unsigned_integral<Size>)
+  class Intersection;
+
+  template <typename Id, typename Size, typename Delay>
+    requires(std::unsigned_integral<Id> && std::unsigned_integral<Size> &&
+             std::unsigned_integral<Delay>)
+  class TrafficLight;
+
+  template <typename Id, typename Size>
+    requires(std::unsigned_integral<Id> && std::unsigned_integral<Size>)
+  class Roundabout;
+
   // define is_node type trait
   template <typename T>
   struct is_node : std::false_type {};
@@ -25,6 +38,43 @@ namespace dsm {
 
   template <typename Id, typename Size>
   struct is_node<std::unique_ptr<Node<Id, Size>>> : std::true_type {};
+
+  // TODO: this is bad, I'll rework the type-traits
+  template <typename Id, typename Size>
+  struct is_node<Intersection<Id, Size>> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<const Intersection<Id, Size>> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<const Intersection<Id, Size>&> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<std::unique_ptr<Intersection<Id, Size>>> : std::true_type {};
+
+  template <typename Id, typename Size, typename Delay>
+  struct is_node<TrafficLight<Id, Size, Delay>> : std::true_type {};
+
+  template <typename Id, typename Size, typename Delay>
+  struct is_node<const TrafficLight<Id, Size, Delay>> : std::true_type {};
+
+  template <typename Id, typename Size, typename Delay>
+  struct is_node<const TrafficLight<Id, Size, Delay>&> : std::true_type {};
+
+  template <typename Id, typename Size, typename Delay>
+  struct is_node<std::unique_ptr<TrafficLight<Id, Size, Delay>>> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<Roundabout<Id, Size>> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<const Roundabout<Id, Size>> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<const Roundabout<Id, Size>&> : std::true_type {};
+
+  template <typename Id, typename Size>
+  struct is_node<std::unique_ptr<Roundabout<Id, Size>>> : std::true_type {};
 
   template <typename T>
   inline constexpr bool is_node_v = is_node<T>::value;
