f0130ddf53671ddf95f592eaab840373a96e4f2b
Grufoony <gregorio_2000@hotmail.it>
2024-11-12 12:12:18 +0100

    Update version

--- src/dsm/dsm.hpp
+++ src/dsm/dsm.hpp
@@ -5,7 +5,7 @@
 #include <format>
 
 static constexpr uint8_t DSM_VERSION_MAJOR = 2;
-static constexpr uint8_t DSM_VERSION_MINOR = 0;
+static constexpr uint8_t DSM_VERSION_MINOR = 1;
 static constexpr uint8_t DSM_VERSION_PATCH = 0;
 
 #define DSM_VERSION \
