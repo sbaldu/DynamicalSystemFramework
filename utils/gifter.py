10afbfc3233154857d0d1dfef1227eded91e0c3f
Grufoony <gregorio_2000@hotmail.it>
2024-11-15 14:55:33 +0100

    Improved cli

--- utils/gifter.py
+++ utils/gifter.py
@@ -20,9 +20,6 @@ import contextily as ctx
 from functions import create_graph_from_adj
 
 # Constants
-TIME_BEGIN = None  # None to take the last frames
-N_FRAMES = 10
-TIME_GRANULARITY = 300
 COLORMAP = colormaps["RdYlGn_r"]
 
 OUTPUT_FILE_NAME = "evolution.gif"
@@ -117,6 +114,27 @@ if __name__ == "__main__":
         required=False,
         help="Use basemap for plotting.",
     )
+    parser.add_argument(
+        "--time-granularity",
+        type=int,
+        default=300,
+        required=False,
+        help="Time granularity in seconds.",
+    )
+    parser.add_argument(
+        "--time-begin",
+        type=int,
+        default=None,
+        required=False,
+        help="Time to begin plotting. If None, it will take the last N_FRAMES.",
+    )
+    parser.add_argument(
+        "--n-frames",
+        type=int,
+        default=10,
+        required=False,
+        help="Number of frames to generate.",
+    )
     args = parser.parse_args()
     # Load the graph
     # read the adjacency matrix discarding the first line
@@ -158,14 +176,14 @@ if __name__ == "__main__":
     df = df.iloc[:, :-1]
 
     # take only rows with index % 300 == 0
-    df = df[df.index % TIME_GRANULARITY == 0]
-    if TIME_BEGIN is not None:
-        df = df[df.index > TIME_BEGIN]
+    df = df[df.index % args.time_granularity == 0]
+    if args.time_begin is not None:
+        df = df[df.index > args.time_begin]
         # take N_FRAMES from the beginning
-        df = df.head(N_FRAMES)
+        df = df.head(args.n_frames)
     else:
         # take the last N_FRAMES
-        df = df.tail(N_FRAMES)
+        df = df.tail(args.n_frames)
 
     # check if the temp_img folder exists, if not create it
     pathlib.Path("./temp_img").mkdir(parents=True, exist_ok=True)
@@ -196,7 +214,7 @@ if __name__ == "__main__":
         frames = [Image.open(result[1]) for result in tqdm(results)]
 
         # if NFRAMES is 1, save a png image
-        if N_FRAMES == 1:
+        if args.n_frames == 1:
             frames[0].save(OUTPUT_FILE_NAME.replace(".gif", ".png"), format="PNG")
         else:
             # Save into a GIF file that loops forever
