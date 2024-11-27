2ac19915510be04e87f4ab2371874d814219253d
Grufoony <gregorio_2000@hotmail.it>
2024-11-27 17:22:52 +0100

    Formatting

--- src/dsm/utility/Typedef.hpp
+++ src/dsm/utility/Typedef.hpp
@@ -11,13 +11,13 @@ namespace dsm {
   using Time = uint64_t;
 
   enum Direction : uint8_t {
-    RIGHT = 0, // delta < 0
-    STRAIGHT = 1, // delta == 0
-    LEFT = 2, // delta > 0
-    UTURN = 3, // std::abs(delta) > std::numbers::pi
+    RIGHT = 0,     // delta < 0
+    STRAIGHT = 1,  // delta == 0
+    LEFT = 2,      // delta > 0
+    UTURN = 3,     // std::abs(delta) > std::numbers::pi
     RIGHTANDSTRAIGHT = 4,
     LEFTANDSTRAIGHT = 5,
     ANY = 6
- };
+  };
 
 };  // namespace dsm
