1da1ba5918ea3800409806b676782a53b5f1fa6e
Grufoony <gregorio_2000@hotmail.it>
2024-12-11 09:47:27 +0100

    Add name attribute to Node class

--- src/dsm/headers/Node.hpp
+++ src/dsm/headers/Node.hpp
@@ -14,6 +14,7 @@
 #include <map>
 #include <format>
 #include <cassert>
+#include <string>
 
 #include "../utility/Logger.hpp"
 #include "../utility/queue.hpp"
@@ -27,22 +28,28 @@ namespace dsm {
   protected:
     Id m_id;
     std::optional<std::pair<double, double>> m_coords;
+    std::string m_name;
     Size m_capacity;
     Size m_transportCapacity;
 
   public:
     /// @brief Construct a new Node object with capacity 1
     /// @param id The node's id
-    explicit Node(Id id) : m_id{id}, m_capacity{1}, m_transportCapacity{1} {}
+    explicit Node(Id id) : m_id{id}, m_name{""}, m_capacity{1}, m_transportCapacity{1} {}
     /// @brief Construct a new Node object with capacity 1
     /// @param id The node's id
     /// @param coords A std::pair containing the node's coordinates (lat, lon)
     Node(Id id, std::pair<double, double> coords)
-        : m_id{id}, m_coords{std::move(coords)}, m_capacity{1}, m_transportCapacity{1} {}
+        : m_id{id},
+          m_coords{std::move(coords)},
+          m_name{""},
+          m_capacity{1},
+          m_transportCapacity{1} {}
 
     Node(Node const& other)
         : m_id{other.m_id},
           m_coords{other.m_coords},
+          m_name{other.m_name},
           m_capacity{other.m_capacity},
           m_transportCapacity{other.m_transportCapacity} {};
     virtual ~Node() = default;
@@ -53,6 +60,9 @@ namespace dsm {
     /// @brief Set the node's coordinates
     /// @param coords A std::pair containing the node's coordinates (lat, lon)
     void setCoords(std::pair<double, double> coords) { m_coords = std::move(coords); }
+    /// @brief Set the node's name
+    /// @param name The node's name
+    void setName(std::string name) { m_name = std::move(name); }
     /// @brief Set the node's capacity
     /// @param capacity The node's capacity
     virtual void setCapacity(Size capacity) { m_capacity = capacity; }
@@ -65,6 +75,9 @@ namespace dsm {
     /// @brief Get the node's coordinates
     /// @return std::optional<std::pair<double, double>> A std::pair containing the node's coordinates
     const std::optional<std::pair<double, double>>& coords() const { return m_coords; }
+    /// @brief Get the node's name
+    /// @return std::string The node's name
+    const std::string& name() const { return m_name; }
     /// @brief Get the node's capacity
     /// @return Size The node's capacity
     Size capacity() const { return m_capacity; }
