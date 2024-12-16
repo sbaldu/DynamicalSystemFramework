be1b03ba69cbc552c35f04904c0b46e497a883d0
Grufoony <gregorio_2000@hotmail.it>
2024-12-16 15:24:33 +0100

    Continuing

--- src/dsm/headers/TrainDynamics.hpp
+++ src/dsm/headers/TrainDynamics.hpp
@@ -6,7 +6,13 @@
 namespace dsm {
   class TrainDynamics : public Dynamics<Train> {
   private:
+    void m_evolveStreet(const std::unique_ptr<Street>& pStreet, bool reinsertAgents) final;
+    bool m_evolveNode(const std::unique_ptr<Node>& pNode) final;
+    void m_evolveAgents() final;
   public:
     TrainDynamics(Graph& graph, std::optional<unsigned int> seed);
+
+    void setAgentSpeed(Id agentId) final;
+    void evolve(bool reinsertAgents = false) final;
   };
 }  // namespace dsm
